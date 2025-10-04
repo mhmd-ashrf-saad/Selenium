16ac0b4349f377e6f9a9ecf62466187dcd30af2d
Mohamed Ashraf <144217537+mhmd-ashrf-saad@users.noreply.github.com>
2025-10-04 19:02:55 +0300

    test: added first test case navigateToGoogle

--- src/test/java/BrowserActions.java
+++ src/test/java/BrowserActions.java
@@ -1,5 +1,13 @@
-import  org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-public class BrowserActions {
+import org.testng.annotations.Test;
 
+public class BrowserActions {
+    WebDriver driver;
+
+    @Test
+    public void navigateToGoogle() {
+        driver = new ChromeDriver();
+        driver.get("https://www.Google.com/");
+    }
 }
